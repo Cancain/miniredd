b84f61e0ce8fdebd9cd5b2da247e0a88baad27dc
Cancain <eriksson.tomas@gmail.com>
2019-05-23 17:44:42 +0200

    began work on the restructure of the api

---
+++ backend/Libraries/Database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+//Connection to database
+mongoose
+  .connect("mongodb://localhost/miniredd", {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("Connected to mongoDB...");
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ backend/oldApp.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// //Connection to database
+// mongoose
+//   .connect("mongodb://localhost/miniredd", {
+//     useNewUrlParser: true
+//   })
+//   .then(() => {
+//     console.log("Connected to mongoDB...");
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+//Load the post controller
+
+//Load the post schema
+require("./Models/Posts/Post");
+const Post = mongoose.model("post");
+
+//Index route
+app.get("/", (req, res) => {
+  const post = {
+    title: "Test",
+    content: "This is a test, warning!"
+  };
+
+  new Post(post)
+    .save()
+    .then(console.log("post added"))
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+//Get all posts route
+app.get("/posts", (req, res) => {
+  //Setting up headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
+
+  Post.find({})
+    .then(posts => {
+      console.log("All posts loaded from database...");
+      res.end(JSON.stringify(posts));
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
