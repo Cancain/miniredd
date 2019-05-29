99c3dc0b3c116425b8c380680ed0d5829138302d
Cancain <eriksson.tomas@gmail.com>
2019-05-29 13:04:49 +0200

    added a menu button for mobile

--- frontend/src/Containers/Layout/Layout.js
+++ frontend/src/Containers/Layout/Layout.js
@@ -4,14 +4,20 @@ import Content from "../Content/Content";
 import Router from "../../hoc/Router/Router";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
+import MobileMenuBtn from "../../Components/UI/MobileMenuBtn/MobileMenuBtn";
 
 import "./Layout.css";
 
 function Layout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <div className="App">
+      <MobileMenuBtn clicked={() => toggleSidebar()} />
       <Sidebar open={sidebarOpen} />
       <Content>
         <Header />
