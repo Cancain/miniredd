c769dbd6736f3cdf5a529c2ac7a7255f2b7a1a88
Cancain <eriksson.tomas@gmail.com>
2019-05-29 13:52:03 +0200

    added links to the sidbar mobile navigation

--- frontend/src/Containers/Navigation/Navigation.js
+++ frontend/src/Containers/Navigation/Navigation.js
@@ -1,11 +1,32 @@
 import React, { Fragment } from "react";
 
-// import style from "./Navigation.module.css";
+import RouterLink from "../../Components/UI/RouterLink/RouterLink";
+
+import style from "./Navigation.module.css";
 
 const Navigation = props => {
+  const navItems = [
+    { text: "Home", link: "/" },
+    { text: "Add Post", link: "/addpost" }
+  ];
+
+  const renderNavItems = navItems.map(item => {
+    return (
+      <RouterLink
+        isButton
+        width="90%"
+        margin="0 0 10px 0"
+        key={item.text}
+        text={item.text}
+        to={item.link}
+      />
+    );
+  });
+
   const renderNavigation = (
     <Fragment>
       <h2>Navigation</h2>
+      <div className={style.NavItems}>{renderNavItems}</div>
     </Fragment>
   );
 
